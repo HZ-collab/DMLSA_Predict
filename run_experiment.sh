44c8d94e4cec1975c474ba0b0126b8540b836e51
HZ-collab <2150703225@qq.com>
2024-07-23 23:18:59 +0800

    Add files via upload

---
+++ run_experiment.sh
@@ -0,0 +1,18 @@
+# python Run.py --policy_noise 0 --dataset 'MetrLA' --seed 0
+# python Run.py --policy_noise 0 --dataset 'MetrLA' --seed 16
+# python Run.py --policy_noise 0 --dataset 'MetrLA' --seed 27
+# python Run.py --policy_noise 0 --dataset 'MetrLA' --seed 35
+# python Run.py --policy_noise 0 --dataset 'MetrLA' --seed 44
+# python Run.py --policy_noise 0 --dataset 'MetrLA' --seed 50
+# python Run.py --policy_noise 0 --dataset 'MetrLA' --seed 61
+# python Run.py --policy_noise 0 --dataset 'PEMSD7' --seed 0
+# python Run.py --policy_noise 0 --dataset 'PEMSD7' --seed 16
+# python Run.py --policy_noise 0 --dataset 'PEMSD7' --seed 27
+# python Run.py --policy_noise 0 --dataset 'PEMSD7' --seed 35
+# python Run.py --policy_noise 0 --dataset 'PEMSD7' --seed 44
+# python Run.py --policy_noise 0 --dataset 'PEMSD7' --seed 50
+# python Run.py --policy_noise 0 --dataset 'PEMSD7' --seed 61
+
+python Run.py --policy_noise 0 --dataset 'PEMSD7' --type 'offSpace'
+python Run.py --policy_noise 0 --dataset 'PEMSD7' --type 'offShort'
+python Run.py --policy_noise 0 --dataset 'PEMSD7' --type 'offLong'
